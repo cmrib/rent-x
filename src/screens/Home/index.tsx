15d75d24284cce8626c17505add6c8ab5d0f5a5a
cmrib <ciceromirandar@gmail.com>
2022-08-24 09:33:13 -0300

    create header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Container, Title } from './styles'
+import React from 'react';
+import { Container, Header, Title } from './styles';
+import { StatusBar } from 'react-native';
+import * as Svg from 'react-native-svg'
+
+
 
 export function Home() {
     return (
         <Container>
-            <Title>Cicero</Title>
+            <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+            <Header>
+                <Title>Cicero</Title>
+            </Header>
         </Container>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,20 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-    background-color: ${({ theme }) => theme.colors.success};
+    flex: 1;   
+    background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-    font-family: ${({ theme }) => theme.fonts.primary_500};
-    font-size: 32px;
+export const Header = styled.View`    
+    background-color: ${({ theme }) => theme.colors.header};
+    height: 113px;
+    width: 100%;
+    justify-content: flex-end;
+    padding: 0px 0px 16px 32px;
+`;
+
+export const Title = styled.Text`    
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: 24px;
 `;
