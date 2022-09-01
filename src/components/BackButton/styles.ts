a81a2f0d7b6f7008bafc152d3a8df3a1d935d7c5
cmrib <ciceromirandar@gmail.com>
2022-09-01 08:33:37 -0300

    create ImageSlider component

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -1,7 +1,9 @@
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import styled from "styled-components/native"
 
 
 export const Container = styled.TouchableOpacity`
+   
 
 `;
 
--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,13 +1,20 @@
-import { Container, Header } from './styles';
+import { Container, Header, CarImages } from './styles';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+
+
 
 export function CarDetails() {
     return (
         <Container>
             <Header>
-                <BackButton onPress={() => { }} />
+                <BackButton onPress={() => { console.log('cicero') }} />
             </Header>
 
+            <CarImages>
+                <ImageSlider imagesUrl={['https://cdn.wheel-size.com/automobile/body/audi-rs5-2020-2022-1613028936.4473815.png']} />
+
+            </CarImages>
         </Container>
     )
 }
