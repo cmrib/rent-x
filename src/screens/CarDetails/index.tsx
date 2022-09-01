e862058da18b9400e738bbc2de95ca38b98c7cb2
cmrib <ciceromirandar@gmail.com>
2022-09-01 10:30:18 -0300

    create Acessory component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,14 @@
-import { Container, Header, CarImages, Content, Details, Description, Brand, Name, Rent, Period, Price, About } from './styles';
+import { Container, Header, CarImages, Content, Details, Description, Brand, Name, Rent, Period, Price, Acessories, About } from './styles';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory'
+
+import speedSvg from '../../assets/speed.svg'
+import accelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
 
 
 
@@ -28,9 +36,19 @@ export function CarDetails() {
                     </Rent>
                 </Details>
 
+                <Acessories>
+                    <Acessory name="380Km/h" icon={speedSvg} />
+                    <Acessory name="3.2s" icon={accelerationSvg} />
+                    <Acessory name="800 HP" icon={forceSvg} />
+                    <Acessory name="Gasolina" icon={gasolineSvg} />
+                    <Acessory name="Auto" icon={exchangeSvg} />
+                    <Acessory name="2" icon={peopleSvg} />
+                </Acessories>
+
                 <About>
                     Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
                 </About>
+
             </Content>
         </Container>
     )
