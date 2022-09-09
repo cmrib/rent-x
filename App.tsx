fec8b2a916e4f2374571a4674af11cd5d68818a3
cmrib <ciceromirandar@gmail.com>
2022-09-09 11:31:55 -0300

    create SchedulingDetails screen

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from 'styled-components'
 import theme from './src/styles/theme';
 import { CarDetails } from './src/screens/CarDetails';
 import { Scheduling } from './src/screens/Scheduling';
+import { SchedulingDetails } from './src/screens/SchedulingDetails';
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
@@ -27,7 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <SchedulingDetails />
     </ThemeProvider>
   );
 }
---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,98 @@
+import { Container, Header, CarImages, Content, Details, Description, Brand, Name, Rent, Period, Price, Acessories, RentalPeriod, CalendarIcon, DateInfo, DateTitle, DateValue, RentalPrice, RentalPriceLabel, RentalPriceDetails, RentalPriceQuota, RentalPriceTotal, Footer } from './styles';
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory';
+import { Button } from '../../components/Button';
+import { Feather } from '@expo/vector-icons'
+
+import speedSvg from '../../assets/speed.svg'
+import accelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components/native';
+
+
+export function SchedulingDetails() {
+
+    const theme = useTheme()
+
+    return (
+
+        <Container>
+            <Header>
+                <BackButton onPress={() => { console.log('cicero') }} />
+            </Header>
+
+            <CarImages>
+                <ImageSlider imagesUrl={['https://cdn.wheel-size.com/automobile/body/audi-rs5-2020-2022-1613028936.4473815.png']} />
+            </CarImages>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+
+                <Acessories>
+                    <Acessory name="380Km/h" icon={speedSvg} />
+                    <Acessory name="3.2s" icon={accelerationSvg} />
+                    <Acessory name="800 HP" icon={forceSvg} />
+                    <Acessory name="Gasolina" icon={gasolineSvg} />
+                    <Acessory name="Auto" icon={exchangeSvg} />
+                    <Acessory name="2 pessoas" icon={peopleSvg} />
+                </Acessories>
+
+                <RentalPeriod>
+                    <CalendarIcon>
+                        <Feather
+                            name="calendar"
+                            size={RFValue(24)}
+                            color={theme.colors.shape}
+                        />
+                    </CalendarIcon>
+
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+                        <DateValue>18/06/2021</DateValue>
+                    </DateInfo>
+
+                    <Feather
+                        name="chevron-right"
+                        size={RFValue(10)}
+                        color={theme.colors.text}
+                    />
+
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+                        <DateValue>18/06/2021</DateValue>
+                    </DateInfo>
+                </RentalPeriod>
+
+                <RentalPrice>
+                    <RentalPriceLabel>TOTAL</RentalPriceLabel>
+                    <RentalPriceDetails>
+                        <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+                        <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+                    </RentalPriceDetails>
+                </RentalPrice>
+
+            </Content>
+
+            <Footer>
+                <Button title="Confirmar" />
+            </Footer>
+
+        </Container>
+
+    )
+}
