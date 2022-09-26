cf8d489434babbe8d5c69d5c660276667dc59635
cmrib <ciceromirandar@gmail.com>
2022-09-26 14:43:06 -0300

    create StatusBar at Scheduling screen

--- src/screens/SchedulingComplete/index.tsx
+++ src/screens/SchedulingComplete/index.tsx
@@ -1,5 +1,5 @@
 import { Container, Content, Title, Message, Footer } from './styles';
-import { useWindowDimensions } from 'react-native'
+import { useWindowDimensions, StatusBar } from 'react-native'
 import LogoSvg from '../../assets/logo_background_gray.svg';
 import DoneSvg from '../../assets/done.svg';
 import { ConfirmButton } from '../../components/ConfirmButton';
@@ -11,6 +11,8 @@ export function SchedulingComplete() {
     return (
 
         <Container>
+
+            <StatusBar barStyle='light-content' backgroundColor="transparent" translucent />
             <LogoSvg width={width} />
 
             <Content>
